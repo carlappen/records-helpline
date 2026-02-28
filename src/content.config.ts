e1dca8329a59757f5d4bca020496b20db1590c70
Carl Appen <carlappen@gmail.com>
2026-02-28 14:24:55 -0500

    again

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,3 +1,18 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const blog = defineCollection({
+	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			heroImage: image().optional(),
+		}),
+});
+
 const resources = defineCollection({
 	loader: glob({ base: './src/content/resources', pattern: '**/*.{md,mdx}' }),
 	schema: z.object({
@@ -9,3 +24,5 @@ const resources = defineCollection({
 		heroImage: z.string().optional(),
 	}),
 });
+
+export const collections = { blog, resources };
